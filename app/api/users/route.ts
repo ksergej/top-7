c5e3b8a0bb8f328f11b5f588c78dcb857df334dd
Sergej Karakcheyev <sergejkarakcheyev@Sergejs-MacBook-Pro-4.local>
2025-08-13 11:17:54 +0200

    REST Api funktioniert

---
+++ app/api/users/route.ts
@@ -0,0 +1,27 @@
+import {NextRequest, NextResponse} from "next/server";
+import {prisma} from "@/prisma/prisma-client";
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+
+  return NextResponse.json(users);
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  let user;
+  try {
+    user = await prisma.user.create({data});
+  } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    if (error.code === 'P2002') {
+      return NextResponse.json(
+        {error: `Duplicate value for field: ${error.meta.target}`},
+        {status: 400}
+      );
+    }
+    console.error('Error creating user:', error);
+    throw error;
+  }
+  return NextResponse.json(user);
+}
