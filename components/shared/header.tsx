ae3143328ca3fef316f2d556c1df0021da3a992b
Sergej Karakcheyev <sergejkarakcheyev@Sergejs-MacBook-Pro-4.local>
2025-08-15 16:53:08 +0200

    setFocused Fixed in header.tsx mit https://streamich.github.io/react-use/?path=/story/ui-useclickaway--docs

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,47 +1,53 @@
 import React from 'react';
 import Image from 'next/image';
 
-import { Container } from './container';
-import { Button } from '../ui/button';
+import {Container} from './container';
+import {Button} from '../ui/button';
 import {ArrowRight, ShoppingCart, User} from 'lucide-react';
-import { SearchInput } from './search-input';
-import { cn } from '@/lib/utils';
-import { CartDrawer } from './cart-drawer';
+import {SearchInput} from './search-input';
+import {cn} from '@/lib/utils';
+import {CartDrawer} from './cart-drawer';
+import Link from "next/link";
 
 interface Props {
   className?: string;
 }
 
-export const Header: React.FC<Props> = ({ className }) => {
+export const Header: React.FC<Props> = ({className}) => {
   return (
     <header className={cn('border-b border-gray-100', className)}>
       <Container className="flex items-center justify-between py-8">
-        <div className="flex items-center gap-4">
-          <Image src="/logo2.png" width={130} height={100} alt="Logo" />
-          <div>
-            <h1 className="text-2xl uppercase font-black">Top 7</h1>
-            <p className="text-sm text-gray-400 leading-3">best reviews</p>
+
+        <Link href="/">
+
+          <div className="flex items-center gap-4">
+            <Image src="/logo2.png" width={130} height={100} alt="Logo"/>
+            <div>
+              <h1 className="text-2xl uppercase font-black">Top 7</h1>
+              <p className="text-sm text-gray-400 leading-3">best reviews</p>
+            </div>
           </div>
-        </div>
+        </Link>
 
         <div className="mx-10 flex-1">
-          <SearchInput />
+          <SearchInput/>
         </div>
 
         <div className="flex items-center gap-3">
-          <Button variant="outline" className="flex items-center gap-1" >
+          <Button variant="outline" className="flex items-center gap-1">
             <User size={16}/>
             Anmelden</Button>
 
           <CartDrawer>
             <Button className="group relative">
               <b>520 ₽</b>
-              <span className="h-full w-[1px] bg-white/30 mx-3" />
+              <span className="h-full w-[1px] bg-white/30 mx-3"/>
               <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
-                <ShoppingCart className="h-4 w-4 relative" strokeWidth={2} />
+                <ShoppingCart className="h-4 w-4 relative" strokeWidth={2}/>
                 <b>3</b>
               </div>
-              <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+              <ArrowRight
+                className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"/>
             </Button>
           </CartDrawer>
         </div>
--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -2,16 +2,33 @@
 
 import { Search } from 'lucide-react';
 import React from 'react';
+import {cn} from "@/lib/utils";
+import {useClickAway} from "react-use";
 
-export const SearchInput = () => {
+interface Props {
+  className?: string;
+}
+
+export const SearchInput = ({className}) => {
+  const [focused, setFocused] = React.useState(false);
+  const ref = React.useRef(null);
+
+  useClickAway(ref, () => {
+    setFocused(false);
+  });
+
   return (
     <>
-      <div className="flex rounded-2xl flex-1 justify-between relative h-11">
+      {focused && <div className={cn('fixed top-0 left-0 bottom-0 right-0 bg-black/50 z-30', className)} />}
+      <div
+        ref={ref}
+        className={cn('flex rounded-2xl flex-1 justify-between relative h-11', className )}>
         <Search className="absolute top-1/2 translate-y-[-50%] left-3 h-5 text-gray-400" />
         <input
-          className="rounded-2xl outline-none w-full bg-gray-50 pl-11"
+          className="rounded-2xl outline-none w-full bg-gray-100 pl-11 z-30"
           type="text"
           placeholder="Ich suche ..."
+          onFocus={() => setFocused(true)}
         />
       </div>
     </>
