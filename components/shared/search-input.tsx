b9c5c142163387ec14425be7251091526d247fa1
Sergej Karakcheyev <sergejkarakcheyev@Sergejs-MacBook-Pro-4.local>
2025-08-15 20:07:33 +0200

    Server Call in search-input.tsx funktioniertg add -ug add -u

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -6,6 +6,7 @@ import {cn} from "@/lib/utils";
 import {useClickAway} from "react-use";
 import Link from "next/link";
 import {Api} from "@/services/api-client";
+import {Product} from "@prisma/client";
 
 interface Props {
   className?: string;
@@ -14,6 +15,7 @@ interface Props {
 export const SearchInput: React.FC<Props> = ({className}) => {
   const [searchQuery, setSearchQuery] = React.useState('');
   const [focused, setFocused] = React.useState(false);
+  const [products, setProducts] = React.useState<Product[]>([]);
   const ref = React.useRef(null);
 
   useClickAway(ref, () => {
@@ -21,7 +23,7 @@ export const SearchInput: React.FC<Props> = ({className}) => {
   });
 
   React.useEffect(() => {
-    Api.products.search(searchQuery);
+    Api.products.search(searchQuery).then(items => setProducts(items));
   }, [searchQuery]);
 
   return (
@@ -44,12 +46,19 @@ export const SearchInput: React.FC<Props> = ({className}) => {
           className={cn("absolute w-full bg-white rounded-2xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
             focused && 'visible opacity-100 top-12',
           )}>
-          <Link className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10" href="/product/1">
-            <img className="rounded-sm h-8 w-8"
-                 src="https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp"
-                 alt="Pizza 1"/>
-            <span> Pizza 1 </span>
-          </Link>
+          {products.map((product) => (
+            <Link
+              key={product.id}
+              className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
+              href={`/products/${product.id}`}>
+              <img className="rounded-sm h-8 w-8"
+                   src={product.imageUrl}
+                   alt={product.name}
+              />
+              <span>{product.name}</span>
+            </Link>
+
+          ))}
         </div>
 
       </div>
--- services/products.ts
+++ services/products.ts
@@ -2,6 +2,6 @@ import {Product} from "@prisma/client";
 import {axiosInstance} from "@/services/instance";
 import {ApiRoutes} from "@/services/constants";
 
-export const search = async (query: string) => {
-  return (await axiosInstance.get<Product>(ApiRoutes.SEARCH_PRODUCS,  {params: {query }})).data;
+export const search = async (query: string) : Promise<Product[]> => {
+  return (await axiosInstance.get<Product[]>(ApiRoutes.SEARCH_PRODUCS,  {params: {query }})).data;
 }
