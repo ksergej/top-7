bb4f5419f2c1f0c0a7748349320fcd741fd7c715
Sergej Karakcheyev <sergejkarakcheyev@Sergejs-MacBook-Pro-4.local>
2025-08-16 08:47:02 +0200

    Search funktioniert: app/product/[id] wird angezeigt

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -3,7 +3,7 @@
 import {Search} from 'lucide-react';
 import React from 'react';
 import {cn} from "@/lib/utils";
-import {useClickAway} from "react-use";
+import {useClickAway, useDebounce} from "react-use";
 import Link from "next/link";
 import {Api} from "@/services/api-client";
 import {Product} from "@prisma/client";
@@ -22,9 +22,11 @@ export const SearchInput: React.FC<Props> = ({className}) => {
     setFocused(false);
   });
 
-  React.useEffect(() => {
-    Api.products.search(searchQuery).then(items => setProducts(items));
-  }, [searchQuery]);
+  useDebounce(() => {
+      Api.products.search(searchQuery).then(items => setProducts(items));
+    },
+    250,
+    [searchQuery]);
 
   return (
     <>
@@ -42,7 +44,7 @@ export const SearchInput: React.FC<Props> = ({className}) => {
           onChange={(e) => setSearchQuery(e.target.value)}
         />
 
-        <div
+        { products.length > 0 && <div
           className={cn("absolute w-full bg-white rounded-2xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
             focused && 'visible opacity-100 top-12',
           )}>
@@ -50,7 +52,7 @@ export const SearchInput: React.FC<Props> = ({className}) => {
             <Link
               key={product.id}
               className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
-              href={`/products/${product.id}`}>
+              href={`/product/${product.id}`}>
               <img className="rounded-sm h-8 w-8"
                    src={product.imageUrl}
                    alt={product.name}
@@ -59,7 +61,7 @@ export const SearchInput: React.FC<Props> = ({className}) => {
             </Link>
 
           ))}
-        </div>
+        </div> }
 
       </div>
     </>
