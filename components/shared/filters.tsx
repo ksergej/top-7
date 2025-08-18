ac864ec0a38f02e9d76d4e2a43807ceb97ae9213
Sergej Karakcheyev <sergejkarakcheyev@Sergejs-MacBook-Pro-4.local>
2025-08-18 14:15:23 +0200

    Price RangeSlider fertig

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -13,9 +13,22 @@ interface Props {
   className?: string;
 }
 
+// Video 6.23.50
+interface PriceProps {
+  priceFrom: number;
+  priceTo: number;
+}
+
 export const Filters: React.FC<Props> = ({className}) => {
   const {ingredients, loading, onAddId, selectedIds} = useFilterIngredients();
   const items = ingredients.map((item) => ({value: String(item.id), text: item.name}));
+  const [prices, setPrice ] = React.useState<PriceProps>({priceFrom: 0, priceTo: 1000});
+  const updatePrice = (name: keyof PriceProps, value: number) => {
+    setPrice({
+      ...prices,
+      [name]: value,
+    });
+  };
 
   return (
     <div className={className}>
@@ -29,11 +42,22 @@ export const Filters: React.FC<Props> = ({className}) => {
       <div className={cn('mt-5 border-y  border-y-neutral-100 py-6 pb-7')}>
         <p className="font-bold mb-3">Цена от и до:</p>
         <div className="flex gap-3 mb-5">
-          <Input type="number" placeholder="0" min={0} max={30000} defaultValue={0}/>
-          <Input type="number" min={100} max={30000} placeholder="30000"/>
+          <Input type="number"
+                 placeholder="0" min={0} max={1000}
+                 value={String(prices.priceFrom)}
+                 onChange={(e) => updatePrice('priceFrom', Number(e.target.value))}
+          />
+          <Input type="number" min={100} max={1000}
+                 placeholder="30000"
+                 value={String(prices.priceTo)}
+                 onChange={(e) => updatePrice('priceTo', Number(e.target.value))}
+          />
         </div>
 
-        <RangeSlider min={0} max={5000} step={10} value={[0, 5000]}/>
+        <RangeSlider min={0} max={1000} step={10}
+                     value={[prices.priceFrom, prices.priceTo]}
+                     onValueChange={([priceFrom, priceTo]) => setPrice({priceFrom, priceTo})}
+        />
 
       </div>
 
