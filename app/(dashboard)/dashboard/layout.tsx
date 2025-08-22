072dbc5c88d4eca6196acbe39182f1cd519d59ae
Sergej Karakcheyev <sergejkarakcheyev@Sergejs-MacBook-Pro-4.local>
2025-08-22 13:12:13 +0200

    Video 7.12.43 Делаем main Layout ...

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 export default function DashboardLayout(
   {children,}:
-  {children: React.ReactNode}
+  { children: React.ReactNode }
 ) {
   return (
     <html lang="en">
+    DASHBOARD HEADER
     <body>{children}</body>
     </html>
   )
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,42 +1,20 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Nunito } from "next/font/google";
-import "../globals.css";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono, Nunito} from "next/font/google";
 import {Header} from "@/components/shared";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const nunito = Nunito({
-  subsets: ['latin'],
-  variable: '--font-nunito',
-  weight: ['400', '500', '600', '700', '800', '900'],
-});
 
 export const metadata: Metadata = {
   title: "Top 7 App",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+export default function HomeLayout({
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-          className={nunito.className} >
-      <main className="min-h-screen">
-          <Header />
-          {children}
-      </main>
-      </body>
-    </html>
+    <main className="min-h-screen">
+      <Header/>
+      {children}
+    </main>
   );
 }
