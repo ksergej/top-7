a54af97f41ef76dbe33c04bd2be0d73eafe57669
Sergej Karakcheyev <sergejkarakcheyev@Sergejs-MacBook-Pro-5.local>
2025-09-03 01:12:52 +0200

    Video 12.08.27 - Test mit Postman: PATCH Request : token : 1111

---
+++ app/api/cart/[id]/route.ts
@@ -0,0 +1,44 @@
+import {NextRequest, NextResponse} from 'next/server';
+import {prisma} from "@/prisma/prisma-client";
+import {updateCartTotalAmount} from "@/shared/lib/update-cart-total-amount";
+
+export async function PATCH(req: NextRequest, {params}: { params: { id: string } }) {
+  try {
+
+
+    const id = Number(params.id);
+    const body = await req.json();
+    const token = req.cookies.get('cartToken')?.value;
+
+    if (!token) {
+      return NextResponse.json({error: 'Cart token not found'});
+    }
+
+    const cartItem = await prisma.cartItem.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!cartItem) {
+      return NextResponse.json({error: 'Cart item not found'});
+    }
+
+    await prisma.cartItem.update({
+      where: {
+        id,
+      },
+      data: {
+        quantity: body.quantity,
+      },
+    })
+
+    const updatedUserCart = await updateCartTotalAmount(token);
+    return NextResponse.json(updatedUserCart);
+
+  } catch (err) {
+    console.log('[CART_PATCH] Server error', err);
+    return NextResponse.json({message: ' Не удалось обновить корзину'}, {status: 500});
+  }
+}
+
