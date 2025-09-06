4cadc48388f65903b763b8c905a276d9184fbc33
Sergej Karakcheyev <sergejkarakcheyev@Sergejs-MacBook-Pro-5.local>
2025-09-06 10:36:02 +0200

    14.53.30 - 14.58.00  Доделываем статус состояния в корзине ...

--- shared/store/cart.ts
+++ shared/store/cart.ts
@@ -2,6 +2,7 @@ import {create} from 'zustand';
 import {Api} from "@/shared/services/api-client";
 import {CartStateItem, getCartDetails} from "@/shared/lib/get-cart-details";
 import {CreateCartItemValues} from "@/shared/services/dto/cart.dto";
+import {constFalse} from "effect/Function";
 
 
 export interface CartState {
@@ -45,14 +46,14 @@ export const useCartStore = create<CartState>((set, get) => ({
 
   updateItemQuantity: async (id: number, quantity: number) => {
     try {
-      set({ loading: true, error: false });
+      set({loading: true, error: false});
       const data = await Api.cart.updateItemQuantity(id, quantity);
       set(getCartDetails(data));
     } catch (error) {
       console.error(error);
-      set({ error: true });
+      set({error: true});
     } finally {
-      set({ loading: false });
+      set({loading: false});
     }
   },
   //
@@ -60,27 +61,36 @@ export const useCartStore = create<CartState>((set, get) => ({
 
   removeCartItem: async (id: number) => {
     try {
-      set({ loading: true, error: false });
+      set((state) => ({
+          loading: true,
+          error: false,
+          items: state.items.map((item) => (item.id === id ? {...item, disabled: true} : item))
+        }
+      ));
       const data = await Api.cart.removeCartItem(id);
       set(getCartDetails(data));
     } catch (error) {
-      set({ error: true });
+      set({error: true});
       console.error(error);
     } finally {
-      set({ loading: false });
+      set((state) => ({
+          loading: false,
+          items: state.items.map((item) => (item.id === id ? {...item, disabled: false} : item))
+        }
+      ));
     }
   },
 
   addCartItem: async (values: CreateCartItemValues) => {
     try {
-      set({ loading: true, error: false });
+      set({loading: true, error: false});
       const data = await Api.cart.addCartItem(values);
       set(getCartDetails(data));
     } catch (error) {
       console.error(error);
-      set({ error: true });
+      set({error: true});
     } finally {
-      set({ loading: false });
+      set({loading: false});
     }
   },
 }));
