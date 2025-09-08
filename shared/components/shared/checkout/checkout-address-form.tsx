17a0d9e84b9d47a9df954eb281c5f0b7ee3168e4
Sergej Karakcheyev <sergejkarakcheyev@Sergejs-MacBook-Pro-5.local>
2025-09-08 08:38:11 +0200

    17.21.00 Добавляем render (валидацию) в AdressInput (внешняя форма) Трик mit Controller

--- shared/components/shared/checkout/checkout-address-form.tsx
+++ shared/components/shared/checkout/checkout-address-form.tsx
@@ -1,24 +1,38 @@
+'use client';
 import React from 'react';
-import {AdressInput, FormTextarea, WhiteBlock} from "@/shared/components/shared";
+import {AdressInput, ErrorText, FormTextarea, WhiteBlock} from "@/shared/components/shared";
+import {Controller, useFormContext} from "react-hook-form";
 
 interface Props {
-    className?: string;
+  className?: string;
 }
 
-export const CheckoutAddressForm: React.FC<Props> = ({ className }) => {
-    return (
-       <WhiteBlock title="3. Адрес доставки">
-          <div className="flex flex-col gap-5">
-            <AdressInput />
-            <FormTextarea
-              name="comment"
-              className="text-base"
-              placeholder='Комментарий к заказу'
-              rows={5}
-            />
-          </div>
+export const CheckoutAddressForm: React.FC<Props> = ({className}) => {
 
-        </WhiteBlock>
-    );
+  const address = 'address';
+  const {control} = useFormContext();
+
+  return (
+    <WhiteBlock title="3. Адрес доставки">
+      <div className="flex flex-col gap-5">
+        <Controller
+          control={control}
+          name={address}
+          render={({field, fieldState}) => <>
+            <AdressInput onChange={field.onChange}/>
+            {fieldState.error?.message && <ErrorText text={fieldState.error.message} /> }
+            </>}
+          />
+
+        <FormTextarea
+        name="comment"
+        className="text-base"
+        placeholder='Комментарий к заказу'
+        rows={5}
+        />
+      </div>
+
+    </WhiteBlock>
+  );
 };
 
